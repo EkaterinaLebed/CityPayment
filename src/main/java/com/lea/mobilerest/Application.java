ed6f40651c1546b327b90a2f718b12abc22e2190
Ekaterina Lebed <my@mail.com>
2017-09-23 16:09:57 +0300

    init

--- src/main/java/com/lea/mobilerest/Application.java
+++ src/main/java/com/lea/mobilerest/Application.java
@@ -1,16 +1,12 @@
 package com.lea.mobilerest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    private static Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
-        log.error("NOT_FOUND");
 		SpringApplication.run(Application.class, args);
 	}
 }
